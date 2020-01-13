eebac77e2b2bfcc6492de21b29c1e5deca3c9771
皮落痕 <1012106967@qq.com>
2020-01-13 09:41:12 +0800

    modify

--- src/app/business/home/home.module.ts
+++ src/app/business/home/home.module.ts
@@ -3,9 +3,10 @@ import { ShareModule } from 'src/app/share/share.module';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, TreeComponent],
   imports: [ShareModule, HomeRoutingModule]
 })
 export class HomeModule {}
