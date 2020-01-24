76ec1e922119b95a88e9959b1978ceee7aa2df9f
李晓莹 <xiaoyingli@wistronits.com>
2020-01-24 16:17:46 +0800

    蜂鸟map

--- src/app/business/home/fengmap/fengmap.component.ts
+++ src/app/business/home/fengmap/fengmap.component.ts
@@ -8,26 +8,69 @@ declare const fengmap: any;
   templateUrl: './fengmap.component.html',
   styleUrls: ['./fengmap.component.scss']
 })
+
 export class FengmapComponent implements OnInit {
   constructor() {}
+  // 定义全局map变量
+  map = null;
+
+  // 定义地图ID变量
+  fmapID = 'gdc-9';
+
+  // 定义二维/三维模式变量
+  planarFlag = false;
+
+  rootUrl = environment.baseUrl;
 
   ngOnInit() {
-    const fmapID = 'gdc-9'; // mapId
-    const map = new fengmap.FMMap({
+    this.openMap();
+  }
+
+  // 打开地图
+  openMap() {
+    const mapOptions = {
       container: document.getElementById('fengMap'), // 渲染dom
-      mapServerURL: `${environment.baseUrl}assets/fengmap/${fmapID}`,
+      // 地图数据位置
+      mapServerURL: `${environment.baseUrl}assets/fengmap/${this.fmapID}`,
+      // 主题数据位置
       mapThemeURL: `${environment.baseUrl}assets/fengmap/theme`,
+      // 设置主题
       defaultThemeName: 'gdc-9',
-      defaultMapScaleLevel: 19,
-      defaultControlsPose: fengmap.FMDirection.NORTH,
-      focusAnimateMode: false, // 开启聚焦层切换的动画显示
-      viewModeAnimateMode: false, // 开启2维，3维切换的动画显示
-      moveToAnimateMode: false, // 地图定位跳转动画设置
+      // 初始二维/三维状态,默认3D显示
+      defaultViewMode: fengmap.FMViewMode.MODE_2D,
       appName: 'GDC9', // 开发者应用名称
-      key: 'a7d64a35b5ae31e100e3fa5c64c582e2' // 开发者申请应用下web服务的key
-    });
+      key: 'a7d64a35b5ae31e100e3fa5c64c582e2', // 开发者申请应用下web服务的key，地图应用密钥
+    };
+
+    // 初始化地图对象
+    this.map = new fengmap.FMMap(mapOptions);
 
     // 打开Fengmap服务器的地图数据和主题
-    map.openMapById(fmapID);
+    this.map.openMapById(this.fmapID, error => {
+      // 打印错误信息
+      console.log(error);
+    });
+
+    // 地图加载完成事件
+    this.map.on('loadComplete', () => {
+      // 显示自定义控件按钮
+      document.getElementById('btnsSwitch').style.display = 'block';
+    });
+  }
+
+  changeMode(domObj) {
+    if (this.planarFlag) {
+      // 切换地图为二维模式
+      if (this.map) {
+        this.map.viewMode = fengmap.FMViewMode.MODE_2D;
+      }
+    } else {
+      // 切换地图为三维模式
+      if (this.map) {
+        this.map.viewMode = fengmap.FMViewMode.MODE_3D;
+      }
+    }
+    // 更改状态
+    this.planarFlag = !this.planarFlag;
   }
 }
